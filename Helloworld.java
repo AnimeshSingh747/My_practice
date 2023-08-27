0bcf504f82b4b2cbc4ba5f895f0b8455cf9dd7df
Animesh Singh <csanimeshsingh747@gmail.com>
2023-08-28 02:39:50 +0530

    Operator

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,15 @@
 public class Helloworld {
 
+    static String name = "Animesh"; // Static variable
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        String username = "Animesh Singh"; //Local variable
+
+        int i; //Instance variable
+
+
     }
 }
+
+// Local variable , instance variable , static variable
---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    
+    public static void main(String[] args) {
+        int num =20;
+        num++;
+    }
+}
+//Arithmetic operator  +,/.-.*
+//Assignment =
+//Unary ++ , -- 
+//Relational
+//Logical
+//Tenary
+//Bitwise
