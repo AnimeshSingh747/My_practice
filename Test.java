448cc12f3a2e80efc00b76306d6fa77d51ea687c
Animesh Singh <csanimeshsingh747@gmail.com>
2023-08-30 11:47:05 +0530

    Test 1

---
+++ Test.java
@@ -0,0 +1,48 @@
+public class Test {
+    
+   public static void main(String[] args) {
+
+    //WAP TO DISPLAY TWO NUMBERS AND ADD THAT
+
+    // int a = 12;
+    // int b = 22;
+
+    // int result = a + b;
+    // System.out.println(result);
+
+
+//Calculate the average of three numbers using division and addition operators.
+
+    // int a = 12;
+    // int b = 17;
+    // int c = 22;
+
+    // int result = (a + b + c)/3 ;
+    // System.out.println(result);
+    
+
+// Implement a program to calculate the area of a rectangle using multiplication operator.
+
+// int length = 12;
+// int breadth = 7;
+
+// int result = (length * breadth);
+// System.out.println(result);
+
+// Create a program that calculates the compound interest for a given principal amount, interest rate, and time period. Display the final amount.
+
+
+int principal = 1000;
+int rate = 5;
+int time = 5;
+
+int result = (principal * rate * time)/100;
+System.out.println(result);
+
+
+
+
+
+   }
+
+}
